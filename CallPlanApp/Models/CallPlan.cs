caa41f785c1af1ddf50cfc2e5a2d74e751d7450e
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-05-24 18:19:00 +0530

    new commit

--- CallPlanApp/Models/CallPlan.cs
+++ CallPlanApp/Models/CallPlan.cs
@@ -11,13 +11,17 @@ namespace CallPlanApp.Models
     {
         public int Id { get; set; }
         [DisplayName("NAME")]
+        [Required(ErrorMessage = "This is required.")]
         public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "This is required.")]
         [DisplayName("START DATE")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = false)]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "This is required.")]
         [DisplayName("VISIT TYPE")]
         public string VisitType { get; set; } = null!;
+        [Required(ErrorMessage = "This is required.")]
         [DisplayName("COMPLETE")]
         public bool Complete { get; set; }
     }
